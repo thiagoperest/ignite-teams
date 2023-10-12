e302534cbaf4e59780536f27a00062812eb7ee98
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-12 13:37:26 -0300

    feat: add path mapping

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
-import { Groups } from "./src/screens/Groups";
+import { Groups } from "@screens/Groups";
 
 export default function App() {
   return (
