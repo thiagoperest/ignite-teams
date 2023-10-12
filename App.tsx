d6af3d49c129e95a0b07f3989211f7d274d7a661
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-12 16:34:09 -0300

    feat: add definição de tema do app

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
 import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components";
+import theme from "@theme/index";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" translucent />
       <SafeAreaView style={styles.safeArea}>
         <Groups />
       </SafeAreaView>
-    </>
+    </ThemeProvider>
   );
 }
 
