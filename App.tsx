995978e291142224561b3fcc22074ca2e6f84b1a
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-13 06:18:58 -0300

    feat: add fonte roboto

--- App.tsx
+++ App.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  ActivityIndicator,
+} from "react-native";
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import theme from "@theme/index";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" translucent />
       <SafeAreaView style={styles.safeArea}>
-        <Groups />
+        {fontsLoaded ? <Groups /> : <ActivityIndicator />}
       </SafeAreaView>
     </ThemeProvider>
   );
