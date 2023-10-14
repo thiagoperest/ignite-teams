a5890a0c8ea14b93f3fd0a9be1396a5d225b8893
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-14 12:58:21 -0300

    feat: add componente button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 import { Container } from "./styles";
 
 export function Groups() {
@@ -22,6 +23,8 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
