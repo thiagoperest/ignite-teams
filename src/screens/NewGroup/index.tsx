1c574214504cb6a569fd28ff3d5f4b110b97c964
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-14 13:27:21 -0300

    feat: add componente input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -13,7 +14,8 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="cria a turma para adicionar as pessoas"
         />
-        <Button title="Criar" />
+        <Input />
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
