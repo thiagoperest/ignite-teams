75b6c90e5f9f2f6dc52c426cc2e9fffec32af3d3
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-14 12:39:04 -0300

    feat: add componente list empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,10 +3,11 @@ import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -16,6 +17,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
