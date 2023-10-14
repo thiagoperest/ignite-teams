884a3164be0b52ab80d232682391f8dd73129faa
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-14 14:00:49 -0300

    feat: add deixando o ícone dinâmico

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,13 +1,16 @@
 import { TouchableOpacityProps } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Container, Icon, ButtonIconTypeStyleProps } from "./styles";
 
-import { Container, Icon } from "./styles";
+type Props = TouchableOpacityProps & {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  type?: ButtonIconTypeStyleProps;
+};
 
-type Props = TouchableOpacityProps & {};
-
-export function ButtonIcon({}: Props) {
+export function ButtonIcon({ icon, type = "PRIMARY", ...rest }: Props) {
   return (
-    <Container>
-      <Icon name="home" type="PRIMARY" />
+    <Container {...rest}>
+      <Icon name={icon} type={type} />
     </Container>
   );
 }
