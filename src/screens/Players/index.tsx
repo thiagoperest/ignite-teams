74bff2c40a4cc38aee5fda6b9bd5a050758629e0
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-14 14:30:44 -0300

    feat: add finalizando lista de players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,12 +6,14 @@ import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState(["Rodrigo", "Vini"]);
+  const [players, setPlayers] = useState([]);
 
   return (
     <Container>
@@ -51,7 +53,17 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
